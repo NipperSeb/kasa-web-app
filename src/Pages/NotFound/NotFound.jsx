9cb3f3100d6b696778078a80d3fa4ab51fdf9ed4
NipperSeb <sebastien.pincepoche@wanadoo.fr>
2023-05-26 15:42:03 +0200

    fit: page No Found

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <main className="error">
+      <section className="error__container">
+        <h1 className="error__container--title">404</h1>
+        <p className="error__container--text">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+
+        <div
+          className="error__container--navigate"
+          onClick={() => navigate("/")}
+        >
+          Retourner sur la page d'accueil
+        </div>
+      </section>
+    </main>
+  );
 }
